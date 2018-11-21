e5d0a08ce1115090863401dfc5d05c8437af657b
Paolo Pironi <ppiron@gmail.com>
2018-11-21 15:29:59 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../public/style.css';
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <div className='container mx-auto px-4 py-4'>
+        <h1 className="text-center text-orange-dark ">Ciao Pluto!</h1>
+        <p className='text-green'>Ciao, sono Pluto</p>
+      </div>
+    </React.Fragment>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+// const webpack = require('webpack');
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "/dist/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    // hotOnly: true
+  },
+  // plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    }),
+  ]
+};
