74b5aa47911bbe58d5e4322c1352c643d8fe5463
Paolo Pironi <ppiron@gmail.com>
2018-11-22 12:39:49 +0000

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { useState, useEffect } from 'react'
 import '../public/style.css';
 
 export default function App() {
+  const [projects, setProject] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json[0])
+        setProject(json)
+        return projects.map((project) => {
+          return (
+            <p key={project.id}>{project.user}</p>
+          )
+        })
+      })
+  }, [])
+
   return (
     <React.Fragment>
       <div className='container mx-auto px-4 py-4'>
         <h1 className="text-center text-orange-dark ">Ciao Pluto!</h1>
-        <p className='text-green'>Ciao, sono Pluto</p>
+        <p className='text-green-dark'>Ciao, sono Pluto!</p>
+        {(projects.length > 0) ? <p className='text-blue'>There are {projects.length} users</p> : null}
       </div>
     </React.Fragment>
   )
