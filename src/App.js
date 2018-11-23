5363869e77de4ec7041b4067d84c4270e29751e1
Paolo Pironi <ppiron@gmail.com>
2018-11-23 15:31:53 +0000

    come changes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import '../public/style.css';
 
 export default function App() {
   const [projects, setProject] = useState([])
-
+  let pros
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(json => {
         console.log(json[0])
-        setProject(json)
-        return projects.map((project) => {
-          return (
-            <p key={project.id}>{project.user}</p>
-          )
+        setProject((prevProjects) => {
+          const newProjects = [...prevProjects, ...json]
+          pros = newProjects.map((project) => {
+            return (
+              <p key={project.id} className='text-red-darker text-sm'>{project.name}</p>
+              )
+            })
+          console.log(pros, newProjects)
+          return newProjects
         })
       })
   }, [])
@@ -24,7 +28,10 @@ export default function App() {
       <div className='container mx-auto px-4 py-4'>
         <h1 className="text-center text-orange-dark ">Ciao Pluto!</h1>
         <p className='text-green-dark'>Ciao, sono Pluto!</p>
-        {(projects.length > 0) ? <p className='text-blue'>There are {projects.length} users</p> : null}
+        {(projects.length > 0) ? <p className='text-blue'>There are {projects.length} users:</p> : <p>pippo</p>}
+        {(projects.length > 0) ? 
+          console.log(projects) :
+          <p>pippa</p>}
       </div>
     </React.Fragment>
   )
